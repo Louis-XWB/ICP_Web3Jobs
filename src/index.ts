8e0dbbdcd704040abeeee811d3031a7a04c7cff8
Jayachandra777 <124385565+Jayachandra777@users.noreply.github.com>
2023-07-17 21:39:34 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+// this is a public contact system app that allows user to create a contact profile and also view other users contact
 import { $query, $update, Record, StableBTreeMap, Vec, match, Result, nat64, ic, Opt } from 'azle';
 import { v4 as uuidv4 } from 'uuid';
 
@@ -99,132 +100,80 @@ export function getJob(id: string): Result<Job, string> {
 /**
  * apply a job 
 */
-$update;
+@update;
 export function applyJob(id: string, applicantPayload: ApplicantPayload): Result<Job, string> {
-    return match(jobStorage.get(id), {
-        Some: (job) => {
-            const applicants = job.applicants || [];
+    
+   // Input validation
+   if (!applicantPayload.name || applicantPayload.name.trim() === '') {
+       return Result.Err('Name is required.');
+   }
 
-            // check if the caller has already applied the job
-            if (applicants.length > 0) {
-                const isApplied = applicants.findIndex((applicant) => applicant.owner.toString() === ic.caller().toString())>-1;
-                if (isApplied) {
-                    return Result.Err<Job, string>(`You have already applied the job with id=${id}.`);
-                }
-            }
+   if (!applicantPayload.email || applicantPayload.email.trim() === '') {
+       return Result.Err('Email is required.');
+   }
 
-            const applicant: Applicant = {
-                owner: ic.caller().toString(),
-                name: applicantPayload.name,
-                email: applicantPayload.email,
-                applyAt: ic.time()
-            };
-            applicants.push(applicant);
+   // TODO: validate email format
 
-            const updatedJob: Job = { ...job, applicants, updatedAt: Opt.Some(ic.time()) };
-            jobStorage.insert(job.id, updatedJob);
-            return Result.Ok<Job, string>(updatedJob);
-        },
-        None: () => Result.Err<Job, string>(`couldn't apply the job with id=${id}. Job not found`)
-    });
+   // Update the job with the new applicant
+   const updatedJobOpt = jobStorage.get(id).map(job => {
+       const applicants = job.applicants || [];
+
+       // check if the caller has already applied the job
+       if (applicants.length > 0) {
+           const isApplied = applicants.findIndex((applicant) => applicant.owner.toString() === ic.caller().toString())>-1;
+           if (isApplied) {
+               return Result.Err<Job, string>(`You have already applied the job with id=${id}.`);
+           }
+       }
+
+       const applicant: Applicant = {
+           owner: ic.caller().toString(),
+           name: applicantPayload.name,
+           email: applicantPayload.email,
+           applyAt: ic.time()
+       };
+       applicants.push(applicant);
+
+       const updatedJob: Job = { ...job, applicants, updatedAt: Opt.Some(ic.time()) };
+       jobStorage.insert(job.id, updatedJob);
+       return Result.Ok<Job, string>(updatedJob);
+   });
+
+   // Return the result based on the optional value
+   return match(updatedJobOpt, {
+       Some: (result) => result,
+       None: () => Result.Err<Job, string>(`couldn't apply the job with id=${id}. Job not found`)
+   });
 }
 
 /**
  * cancel applied job
  */
-$update;
+@update;
 export function cancelAppliedJob(id: string): Result<Job, string> {
-    return match(jobStorage.get(id), {
-        Some: (job) => {
-            const applicants = job.applicants || [];
-            // check if the caller has already applied the job
-            if (applicants.length === 0) {
-                return Result.Err<Job, string>(`You have not applied the job with id=${id}.`);
-            } else {
-                const isApplied = applicants.some((applicant) => applicant.owner.toString() === ic.caller().toString());
-                if (!isApplied) {
-                    return Result.Err<Job, string>(`You have not applied the job with id=${id}.`);
-                }
-            }
-
-            const updatedApplicants = applicants.filter((applicant) => applicant.owner.toString() !== ic.caller().toString());
-            const updatedJob: Job = { ...job, applicants: updatedApplicants, updatedAt: Opt.Some(ic.time()) };
-            jobStorage.insert(job.id, updatedJob);
-            return Result.Ok<Job, string>(updatedJob);
-        },
-        None: () => Result.Err<Job, string>(`couldn't apply the job with id=${id}. Job not found`)
-    });
-}
-
-/**
- * publishJob a new job
- */
-$update;
-export function publishJob(payload: JobPayload): Result<Job, string> {
-    const job: Job = {
-        id: uuidv4(),
-        position: payload.position,
-        email: payload.email,
-        skill: "",
-        publisher: ic.caller().toString(),
-        applicants: [],
-        companyName: "",
-        companyUrl: "",
-        description: "",
-        salary: "",
-        location: "",
-        createdAt: ic.time(),
-        updatedAt: Opt.None
-    };
-    jobStorage.insert(job.id, job);
-    return Result.Ok(job);
-}
-
-/**
- * update the info of published job
- */
-$update;
-export function updateJob(id: string, payload: JobPayload): Result<Job, string> {
-    return match(jobStorage.get(id), {
-        Some: (job) => {
-            // check if the caller is the publisher of the job
-            if (job.publisher.toString() !== ic.caller().toString()) {
-                return Result.Err<Job, string>(`You are not the publisher of the job with id=${id}.`);
-            }
-            const updatedJob: Job = { ...job, ...payload, updatedAt: Opt.Some(ic.time()) };
-            jobStorage.insert(job.id, updatedJob);
-            return Result.Ok<Job, string>(updatedJob);
-        },
-        None: () => Result.Err<Job, string>(`couldn't update the job with id=${id}. Job not found`)
-    });
-}
-
-/**
- * delete the published job
- */ 
-$update;
-export function deleteJob(id: string): Result<Job, string> {
-    return match(jobStorage.remove(id), {
-        Some: (deletedJob) => {
-            // check if the caller is the publisher of the job
-            if (deletedJob.publisher.toString() !== ic.caller().toString()) {
-                return Result.Err<Job, string>(`You are not the publisher of the job with id=${id}.`);
-            }
-            return Result.Ok<Job, string>(deletedJob)
-        },
-        None: () => Result.Err<Job, string>(`couldn't delete the job with id=${id}. Job not found.`)
-    });
-}
-
-// a workaround to make uuid package work with Azle
-globalThis.crypto = {
-    getRandomValues: () => {
-        let array = new Uint8Array(32)
-
-        for (let i = 0; i < array.length; i++) {
-            array[i] = Math.floor(Math.random() * 256)
+    // Update the job by removing the caller from the applicants
+    const updatedJobOpt = jobStorage.get(id).map(job => {
+        const applicants = job.applicants || [];
+        // check if the caller has already applied the job
+        if (applicants.length === 0) {
+            return Result.Err<Job, string>(`You have not applied the job with id=${id}.`);
         }
 
-        return array
-    }
+        const index = applicants.findIndex((applicant) => applicant.owner.toString() === ic.caller().toString());
+        if (index === -1) {
+            return Result.Err<Job, string>(`You have not applied the job with id=${id}.`);
+        }
+
+        applicants.splice(index, 1);
+
+        const updatedJob: Job = { ...job, applicants, updatedAt: Opt.Some(ic.time()) };
+        jobStorage.insert(job.id, updatedJob);
+        return Result.Ok<Job, string>(updatedJob);
+    });
+
+    // Return the result based on the optional value
+    return match(updatedJobOpt, {
+        Some: (result) => result,
+        None: () => Result.Err<Job, string>(`couldn't cancel the applied job with id=${id}. Job not found`)
+    });
 }
